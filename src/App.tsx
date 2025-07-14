36dc7331d1bea05b21e0286ba5ccaf6e782c83ff
VuongNQ <vuongnq@firegroup.io>
2025-07-14 09:24:54 +0700

    feat: initialize project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, building, and linting.
    - Included Vite logo SVG and React logo SVG in the public and assets directories respectively.
    - Created main application component (App.tsx) with links to Vite and React.
    - Set up Tailwind CSS in index.css.
    - Configured TypeScript with tsconfig files for app and node environments.
    - Implemented Vite configuration with React and Tailwind CSS plugins.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="w-10 h-10" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="w-10 h-10" alt="React logo" />
+        </a>
+      </div>
+      <h1>Ma Sói</h1>
+    </div>
+  );
+}
+
+export default App;
